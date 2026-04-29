68fd8e5a110bcc8789e46048d182ab138b5e1c6c
Sadik Saifi <sadiksaifi7428@gmail.com>
2026-04-30 02:04:01 +0530

    first commit

---
+++ src/hooks/useCurrencyInfo.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react"
+
+function useCurrencyInfo(currency) {
+    const [data, setData] = useState({})
+    useEffect(() => {
+        const url = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+        fetch(url)
+            .then((res) => res.json())
+            .then((res) => {
+                setData(res[currency.toLowerCase()])
+            })
+    }, [currency])
+
+    return data
+}
+
+export default useCurrencyInfo;
